545befd10dce4f94354d32715ac0aa2cda8eb8ed
5z3wczykj4kub <5z3wczykj4kub@gmail.com>
2022-08-20 14:16:43 +0200

    add friend request response menu

--- components/profile/FriendsStatusButton.tsx
+++ components/profile/FriendsStatusButton.tsx
@@ -1,5 +1,8 @@
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PersonAddDisabledIcon from '@mui/icons-material/PersonAddDisabled';
 import { LoadingButton } from '@mui/lab';
-import { useState } from 'react';
+import { ListItemIcon, Menu, MenuItem } from '@mui/material';
+import { MouseEvent, useRef, useState } from 'react';
 import getFriendsStatusAction from '../../lib/functions/getFriendsStatusAction';
 import getFriendsStatusButtonIconByFriendsStatusAction from '../../lib/functions/getFriendsStatusButtonIconByFriendsStatusAction';
 import { User } from '../../models/User';
@@ -18,13 +21,23 @@ const FriendsStatusButton = ({ profile, user }: FriendsStatusButtonProps) => {
     getFriendsStatusAction(profile, user)
   );
 
+  const [menuAnchorEl, setMenuAnchorEl] = useState<null | HTMLElement>(null);
+  const isMenuOpen = !!menuAnchorEl;
+
+  const handleMenuOpen = (event: MouseEvent<HTMLButtonElement>) =>
+    setMenuAnchorEl(event.currentTarget);
+
+  const handleMenuClose = () => setMenuAnchorEl(null);
+
   const [sendFriendInvite, { isLoading: isSendingFriendInvite }] =
     useSendFriendInviteMutation();
 
   const [cancelFriendInvite, { isLoading: isCancelingFriendInvite }] =
     useCancelFriendInviteMutation();
 
-  const handleLoadingButtonClick = async () => {
+  const handleLoadingButtonClick = async (
+    event: MouseEvent<HTMLButtonElement>
+  ) => {
     try {
       if (friendsStatusAction === 'invite') {
         await sendFriendInvite({
@@ -40,22 +53,70 @@ const FriendsStatusButton = ({ profile, user }: FriendsStatusButtonProps) => {
         }).unwrap();
         setFriendsStatusAction('invite');
       }
+      if (friendsStatusAction === 'respond') {
+        handleMenuOpen(event);
+      }
     } catch (error) {}
   };
 
+  const handleAcceptMenuItemClick = () => {
+    handleMenuClose();
+    /**
+     * TODO:
+     * Handle accepting friend request.
+     */
+    console.log('accept');
+  };
+
+  const handleRejectMenuItemClick = () => {
+    handleMenuClose();
+    /**
+     * TODO:
+     * Handle rejecting friend request.
+     */
+    console.log('reject');
+  };
+
+  const loadingButtonRef = useRef<HTMLButtonElement>(null);
+  const loadingButtonWidth =
+    loadingButtonRef.current?.getBoundingClientRect().width;
+
   return (
-    <LoadingButton
-      variant='outlined'
-      loading={isSendingFriendInvite || isCancelingFriendInvite}
-      loadingPosition='end'
-      endIcon={getFriendsStatusButtonIconByFriendsStatusAction(
-        friendsStatusAction
-      )}
-      onClick={handleLoadingButtonClick}
-      sx={{ flexShrink: 0 }}
-    >
-      {friendsStatusAction}
-    </LoadingButton>
+    <>
+      <LoadingButton
+        ref={loadingButtonRef}
+        variant='outlined'
+        loading={isSendingFriendInvite || isCancelingFriendInvite}
+        loadingPosition='end'
+        endIcon={getFriendsStatusButtonIconByFriendsStatusAction(
+          friendsStatusAction
+        )}
+        onClick={handleLoadingButtonClick}
+        sx={{ flexShrink: 0 }}
+      >
+        {friendsStatusAction}
+      </LoadingButton>
+      <Menu open={isMenuOpen} anchorEl={menuAnchorEl} onClose={handleMenuClose}>
+        <MenuItem
+          onClick={handleAcceptMenuItemClick}
+          sx={{ width: loadingButtonWidth }}
+        >
+          <ListItemIcon>
+            <PersonAddIcon fontSize='small' />
+          </ListItemIcon>
+          Accept
+        </MenuItem>
+        <MenuItem
+          onClick={handleRejectMenuItemClick}
+          sx={{ width: loadingButtonWidth }}
+        >
+          <ListItemIcon>
+            <PersonAddDisabledIcon fontSize='small' />
+          </ListItemIcon>
+          Reject
+        </MenuItem>
+      </Menu>
+    </>
   );
 };
 
