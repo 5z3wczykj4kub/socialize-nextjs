8e78da611268ff78f0628c35bb5adaee81046b42
5z3wczykj4kub <5z3wczykj4kub@gmail.com>
2022-09-06 17:25:54 +0200

    improve profile friends list

--- components/profile/FriendsTab.tsx
+++ components/profile/FriendsTab.tsx
@@ -8,6 +8,7 @@ import {
   useMediaQuery,
   useTheme,
 } from '@mui/material';
+import Link from 'next/link';
 import { User } from '../../models/User';
 
 interface FriendsTabProps {
@@ -21,35 +22,31 @@ const FriendsTab = ({ friends }: FriendsTabProps) => {
   return (
     <Paper elevation={4} sx={{ width: '100%', overflow: 'hidden' }}>
       <Grid container>
-        {friends.map(({ id, firstName, lastName }) => {
-          const fullName = `${firstName} ${lastName}`;
-
-          /**
-           * TODO:
-           * Link to profiles.
-           * Add common friends number.
-           * Handle text overflow.
-           * Remove the dummy array.
-           */
-          return Array(10)
-            .fill(null)
-            .map(() => (
-              <Grid item xs={12} sm={6}>
-                <ListItemButton sx={{ py: 2 }}>
-                  <Stack direction='row' alignItems='center' columnGap={2}>
-                    <Avatar
-                      variant={matches ? 'rounded' : 'circular'}
-                      sx={{
-                        width: { xs: 40, sm: 60 },
-                        height: { xs: 40, sm: 60 },
-                      }}
-                    />
-                    <Typography>{fullName}</Typography>
-                  </Stack>
-                </ListItemButton>
-              </Grid>
-            ));
-        })}
+        {friends.map(({ id, firstName, lastName }) => (
+          <Grid item xs={12} sm={6} key={id}>
+            <Link href={`/users/${id}`}>
+              <ListItemButton href={`/users/${id}`} sx={{ py: 2 }}>
+                <Stack
+                  direction='row'
+                  alignItems='center'
+                  columnGap={2}
+                  sx={{ overflowX: 'hidden' }}
+                >
+                  <Avatar
+                    variant={matches ? 'rounded' : 'circular'}
+                    sx={{
+                      width: { xs: 40, sm: 60 },
+                      height: { xs: 40, sm: 60 },
+                    }}
+                  />
+                  <Typography noWrap>
+                    {firstName} {lastName}
+                  </Typography>
+                </Stack>
+              </ListItemButton>
+            </Link>
+          </Grid>
+        ))}
       </Grid>
     </Paper>
   );
